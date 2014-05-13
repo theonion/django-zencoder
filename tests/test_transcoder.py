deb7f042e1cae603c707690e1b6930ab69ed06db
Chris Sinchok <csinchok@theonion.com>
2014-05-13 18:06:25 -0500

    Test job data storage

--- tests/test_transcoder.py
+++ tests/test_transcoder.py
@@ -5,18 +5,20 @@ from video.models import Job, Video
 
 from httmock import urlmatch, HTTMock, response
 
+SAMPLE_JOB_RESPONSE = {
+    "id": 93541697,
+    "outputs": [{
+        "label": "mp4",
+        "url": "http://example.com/hd.mp4",
+        "id": 24211926
+    }]
+}
+
 
 @urlmatch(netloc=r'app\.zencoder\.com$', path="/api/v2/jobs")
 def zencoder_jobs_mock(url, request):
 
-    content = json.dumps({
-        "id": 93541697,
-        "outputs": [{
-            "label": "mp4",
-            "url": "http://example.com/hd.mp4",
-            "id": 24211926
-        }]
-    })
+    content = json.dumps(SAMPLE_JOB_RESPONSE)
     headers = {'content-type': 'application/json'}
     return response(status_code=201, content=content, headers=headers, request=request)
 
@@ -31,3 +33,4 @@ def test_start_endcode(settings):
         job = Job.objects.start(video)
         assert job.status == Job.IN_PROGRESS
         assert job.job_id == 93541697
+        assert job.data == SAMPLE_JOB_RESPONSE
