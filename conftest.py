9ce934be2bc5051c7366733390472f9a760d9a3b
Chris Sinchok <csinchok@theonion.com>
2014-05-14 14:47:32 -0500

    FUCKIT

---
+++ conftest.py
@@ -0,0 +1,27 @@
+import os
+
+from django.conf import settings
+
+MODULE_ROOT = os.path.dirname(os.path.realpath(__file__))
+
+
+def pytest_configure():
+
+    settings.configure(
+        DATABASES={
+            'default': {
+                'ENGINE': 'django.db.backends.sqlite3',
+                'NAME': ':memory:'
+            }
+        },
+        USE_TZ=True,
+        INSTALLED_APPS=(
+            "zencoder"
+        ),
+        ROOT_URLCONF = "testproject.urls",
+        VIDEO_ENCODING_DIRECTORY = "video",
+        VIDEO_ENCODING_BUCKET = "example_bucket",
+
+        AWS_SECRET_ACCESS_KEY = "12345",
+        AWS_ACCESS_KEY_ID = "abcd1234",
+    )
