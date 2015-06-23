703da8d5fa58b8fc1744261a0bbbbb0e88292f8d
vforgione <v.forgione@gmail.com>
2015-06-23 11:19:17 -0500

    bumped version. added version docs. [ci skip]

--- zencoder/__init__.py
+++ zencoder/__init__.py
@@ -1,1 +1,6 @@
-__version__ = "0.2.63"
+# version format:
+# <MAJOR VERSION>.<MINOR VERSION>.<LAST PASSING BUILD>
+# to bump build number without causing a new build, add `[ci skip]`
+# to the end of your commit message: a la "bumping version [ci skip]"
+
+__version__ = "0.2.66"
