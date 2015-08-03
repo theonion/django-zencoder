98b6466f10fc98789e205ff2946fdb2db191978e
Cameron Lowe <cam.m.lowe@gmail.com>
2015-08-03 10:11:07 -0500

    no south - version

--- zencoder/__init__.py
+++ zencoder/__init__.py
@@ -3,4 +3,4 @@
 # to bump build number without causing a new build, add `[ci skip]`
 # to the end of your commit message: a la "bumping version [ci skip]"
 
-__version__ = "0.2.66"
+__version__ = "0.2.67"
